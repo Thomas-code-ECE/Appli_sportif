b3877f13487c2425dff021ffa057d2929f485a53
Thomas COSTA <61493790+Thomas-code-ECE@users.noreply.github.com>
2020-11-18 17:56:55 +0100

    Version 2.0.0

---
+++ app/src/main/java/com/example/application_running/Course_duo.java
@@ -0,0 +1,56 @@
+package com.example.application_running;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Chronometer;
+import android.os.SystemClock;
+import android.widget.Toast;
+import android.view.*;
+
+public class Course_duo extends AppCompatActivity {
+
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_duo);
+
+        chronometer = findViewById(R.id.chronometer);
+        chronometer.setFormat("Time: %s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if ((SystemClock.elapsedRealtime() - chronometer.getBase()) >= 10000) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    Toast.makeText(Course_duo.this, "Bing!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+    public void startChronometer(View v) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+    public void pauseChronometer(View v) {
+        if (running) {
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+    public void resetChronometer(View v) {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+    }
+
+}
--- app/src/main/java/com/example/application_running/MainActivity.java
+++ app/src/main/java/com/example/application_running/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.application_running;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private Button course_duo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.course_duo= findViewById(R.id.button3);
+
+        course_duo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(),Course_duo.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+
     }
 }
